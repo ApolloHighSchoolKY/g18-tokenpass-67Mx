18f931a41835e0e3848188204509950cd194c045
67Mx <max.dominguez@stu.daviess.kyschools.us>
2026-02-04 17:54:33 +0000

    commit

--- TokenPass.java
+++ TokenPass.java
@@ -1,9 +1,22 @@
 public TokenPass(int playerCount){
   /*  to be answered in part (a) */
-  
+  Random tokenNum = new Random(1, 11);
+  Random playerSet = new Random(0, playerCount-1);
+  ArrayList board = new ArrayList(playerCount);
+  for(int x: board)
+    {
+      x=tokenNum;
+      currentPlayer=playerSet;
+    {
+    return board;
 }
 
 public void distributeCurrentPlayerTokens(){
   /* to be answered in part (b) */
-  
+  Random spot= new Random(0,playerCount-1)
+  int playerTokens = board.get(spot);
+  for(int i=spot+1; playerTokens=0; i++)
+  {
+    board.get(i)
+  }
 }
